45098cb990d0308c596cd81b5afba653c6794b04
Vinisha Gupta <vinishasgupta@gmail.com>
2026-07-12 23:56:02 +0530

    exe7

--- src/exercises/Exercise7.jsx
+++ src/exercises/Exercise7.jsx
@@ -1,14 +1,36 @@
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
-import img from '7.html.jpg'
+import img from './7.html.jpg'
 
 function Exercise7(){
+
+    useGSAP(()=>{
+        gsap.from('.header',{
+            x:-200,
+            duration:1.2
+        })
+
+        gsap.from('.para',{
+            x:200,
+            duration:1.2
+        })
+
+        gsap.from('.button',{
+            scale:1.2,
+            duration:2
+        })
+
+        gsap.to('.image',{
+            opacity:0,
+            duration:2
+        })
+    })
     return(
         <>
-        <h1>Header</h1>
-        <p>ejewhntur ejknkjrnfker kajnfs dsngkjsdiowe ejrje  jdsnfkjfs jdsgnjksd kdsjgnjd sjdkgn sdjn</p>
-        <button>button</button>
-        <img src={img}></img>
+        <h1 className='header'>Header</h1>
+        <p className='para'>ejewhntur ejknkjrnfker kajnfs dsngkjsdiowe ejrje  jdsnfkjfs jdsgnjksd kdsjgnjd sjdkgn sdjn</p>
+        <button className='button border-2 p-3' >button</button>
+        <img src={img} className='w-[100px] h-[100px] image'></img>
         </>
     );
 }
